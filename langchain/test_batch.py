c239863554c6f3cac61e664d336b181e13026af1
Sagar D <sagard.cmagalur@gmail.com>
2025-07-17 00:58:15 +0530

    Work with langchain : invoke, stream, batch

---
+++ langchain/test_batch.py
@@ -0,0 +1,41 @@
+from langchain_ollama import ChatOllama
+
+
+class LangClient:
+    """Class to create ChatOllama client for specific host and model"""
+
+    def __init__(
+        self, base_url="http://localhost:11434", model="llama3.2", temperature=0
+    ):
+        self.base_url = base_url
+        self.model = model
+        self.client = ChatOllama(
+            base_url=base_url, model=model, temperature=temperature
+        )
+
+    def batch_prompts(self, prompt_list):
+        """Method to invoke a batch of prompts and return the batch response"""
+        return self.client.batch(prompt_list)
+
+
+if __name__ == "__main__":
+
+    prompt_list = [
+        "What is the capital of India?",
+        "What is the national sport of India?",
+        "What is the national animal of India?",
+        "What are the major achievements of ISRO?",
+        "Who is the 1st President of India?",
+        "Which are the major cities of India?",
+        "Who are the most famous cricketers from India?",
+    ]
+
+    client = LangClient()
+    response_list = client.batch_prompts(prompt_list)
+
+    for index, prompt, response in zip(
+        range(1, len(prompt_list) + 1), prompt_list, response_list
+    ):
+        print(f"Q{index}. {prompt.capitalize()}")
+        print(response.content)
+        print("\n", "--" * 50, "\n\n", sep='')
---
+++ langchain/test_invoke.py
@@ -0,0 +1,29 @@
+from langchain_ollama import ChatOllama
+
+
+class LangClient:
+    """Class to create a ChatOllama client object"""
+
+    def __init__(
+        self, base_url="http://localhost:11434", model="llama3.2", temperature=0
+    ):
+        """Initiate the ChatOllama client with host, model and temparature and create a client"""
+        self.base_url = base_url
+        self.model = model
+        self.client = ChatOllama(
+            base_url=base_url, model=model, temperature=temperature
+        )
+
+    def invoke_prompt(self, prompt):
+        """Method to invoke a single prompt and get back complete response"""
+        return self.client.invoke(prompt)
+
+
+if __name__ == "__main__":
+
+    prompt = "How many breeds of dogs are found across the world?"
+
+    client = LangClient()
+    response = client.invoke_prompt(prompt)
+
+    print(response.content)
