8df52831c5462125e6aacd8036af4dd15a4a2439
Sagar D <sagard.cmagalur@gmail.com>
2025-07-24 22:42:11 +0530

    combine chains sequentially and in parallel

---
+++ langchain/t7_chain_of_chains.py
@@ -0,0 +1,44 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+llm = ChatOllama(base_url="http://localhost:11434", model="llama3.2", temperature=0)
+
+parser = StrOutputParser()
+
+thesis_statements = [
+    "The fundametal concepts quantum physcis are difficult to graps, even for the mostly advanced students.",
+    "Einstein's theroy of relativity revolutionised undrstanding of space and time, making it clear that they are interconnected.",
+    "The first law of thermodynmics states that energy cannot be created or destoryed, excepting only transformed from one form to another.",
+    "Electromagnetism is one of they four funadmental forces of nature, and it describes the interaction between charged particles.",
+    "In the study of mechanic, Newton's laws of motion provide a comprehensive framework for understading the movement of objects under various forces.",
+]
+
+## Fix the grammer and spelling errors for above thesis statements.
+## Generate a paragraph for each of the thesis statements
+
+spell_correct_template = ChatPromptTemplate.from_template(
+    """\
+Correct any spelling or grammatical errors in the below statement and respond back with the corrected statemnt. \
+Respond on;y with the corrected statment and don'e send any other notes or details.
+Statement : "{statment}"
+"""
+)
+
+spell_correct_chain = spell_correct_template | llm | parser
+
+paragraph_generator_template = ChatPromptTemplate.from_template(
+    """\
+Generate a paragraph related to the statement given below. Use the below statment as the first line of the paragraph. \
+Respond only with paragraph. Don't add any other notes or details in the response.
+Statemnet : "{statemnet}"
+"""
+)
+
+paragraph_generator_chain = paragraph_generator_template | llm | parser
+
+final_chain = spell_correct_chain | paragraph_generator_chain
+
+for paragraph in final_chain.batch(thesis_statements):
+    print(paragraph)
+    print()
---
+++ langchain/t8_parallel_chains.py
@@ -0,0 +1,78 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnableParallel
+from langchain_core.output_parsers import StrOutputParser
+
+llm = ChatOllama(base_url="http://localhost:11434", model="llama3.2", temperature=0)
+
+parser = StrOutputParser()
+
+statements = [
+    "I had a fantastic time hiking up the mountain yesterday.",
+    "The new restaurant downtown serves delicious vegetarian dishes.",
+    "I am feeling quite stressed about the upcoming project deadline.",
+    "Watching the sunset at the beach was a calming experience.",
+    "I recently started reading a fascinating book about space exploration.",
+]
+
+## For the above statements, perform below actions
+## 1. Analyse Overall sentiment
+## 2. Find the Main topic of the statment
+## 3. Generate a Followup question
+## Format the above information along with statement and print the formatted response
+## Use parallel chains where applicable
+
+sentiment_template = ChatPromptTemplate.from_template(
+    """\
+Analyse the sentiment of the below statemnet and respond back in one word as either "Positive" or "Negative".
+Statement : {statment}
+"""
+)
+
+main_topic_template = ChatPromptTemplate.from_template(
+    """\
+Find the main topic under discussion from the below statment and respond back with only the main topic. \
+Don't add any other notes or details in the response.
+Statement : {statment}
+"""
+)
+
+follow_up_template = ChatPromptTemplate.from_template(
+    """\
+Ask a follow up question for the below statemnet to understand the missing information in the statment. \
+Respond with only question. Don't add any other notes or details in the response.
+Statement : {statment}
+"""
+)
+
+sentiment_chain = sentiment_template | llm | parser
+main_topic_chain = main_topic_template | llm | parser
+follow_up_chain = follow_up_template | llm | parser
+
+parallel_chain = RunnableParallel(
+    {
+        "sentiment": sentiment_chain,
+        "main_topic": main_topic_chain,
+        "follow_up": follow_up_chain,
+        "statement": RunnableLambda(lambda statemnt: statemnt.capitalize()),
+    }
+)
+
+custom_output_parser = RunnableLambda(
+    lambda response: (
+        f"Statement : {response['statement']}\n"
+        f"Sentiment : {response['sentiment']}\n"
+        f"Main Topic : {response['main_topic']}\n"
+        f"Follow Up Question : {response['follow_up']}\n"
+    )
+)
+
+final_chain = parallel_chain | custom_output_parser
+
+print(final_chain.get_graph().draw_ascii())
+
+print("\n\n", "--" * 50, "\n\n", sep="")
+
+for response in final_chain.batch(statements):
+    print(response)
+    print()
