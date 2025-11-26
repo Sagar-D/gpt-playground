dbe8ed5b076de70c0b4346a1d585e19f15dcb767
Sagar D <sagard.cmagalur@gmail.com>
2025-11-26 15:37:12 +0530

    fix typos

--- mini_projects/meeting_mom_bot/prompt_manager.py
+++ mini_projects/meeting_mom_bot/prompt_manager.py
@@ -1,7 +1,7 @@
 from langchain_core.prompts import PromptTemplate
 
 MEETING_MOM_PROMPT = PromptTemplate.from_template(
-    """You are a helpful assistant whose job is to understand the meeting transcripts and create meeting summury and Minutes of Meeting (MoM).
+    """You are a helpful assistant whose job is to understand the meeting transcripts and create meeting summary and Minutes of Meeting (MoM).
 Minutes of Meeting should include :
 - Agenda of the meeting
 - Short meeting Summary
@@ -13,15 +13,16 @@ Important Notes :
 1. Do not hallucinate! Create the MoM only based on the transcript provided
 2. Keep the MoM precise and pointer wise.
 3. Don't send any other additional text in the response other than MoM
-4. If there isn't enought data to populate any section of MoM, skip that section.
-5. Based on semantic understading, populate pointers for sections like Key decisions made, Action Items etc
+4. If there isn't enough data to populate any section of MoM, skip that section.
+5. Based on semantic understanding, populate pointers for sections like Key decisions made, Action Items etc
 
 Meeting Transcript : 
 {meeting_transcript}
 
 Response : 
 
-** Meeting MoM **
+** Meeting MoM ** :
+
 """
 )
 
